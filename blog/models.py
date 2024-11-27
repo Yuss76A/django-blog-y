cf9d8623ace8676344a0fb7c2e957a7436033ba9
Yusein Ali <yuse_91yus@hotmail.com>
2024-11-27 23:11:29 +0000

    Add more classes to the models.py file

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,8 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
+    updated_on = models.DateTimeField(auto_now=True)
+    excerpt = models.TextField(blank=True, null=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
