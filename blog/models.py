079c9f747c71e68384649f25beb53a5fc9054aa5
Yusein Ali <yuse_91yus@hotmail.com>
2024-12-02 21:39:32 +0000

    generic post view

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
@@ -13,7 +11,6 @@ class Post(models.Model):
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -25,13 +22,19 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,
-                             related_name="comments")
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments_author"
+    )
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ["created_on"]
