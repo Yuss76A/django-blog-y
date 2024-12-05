863439e296bba1509b68d3b5d66b9b275a5a7055
Yusein Ali <yuse_91yus@hotmail.com>
2024-12-05 22:57:35 +0000

    Update the models.py file in the about directory

--- blog/models.py
+++ blog/models.py
@@ -42,12 +42,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
-
-
-class about(models.Model):
-    title = models.CharField(max_length=200)
-    updated_on = models.DateTimeField(auto_now = True)
-    content = models.TextField()
-    
-    def __str__(self):
-        return self.title
