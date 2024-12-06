bde3db5ac5b70e9fe4eb888b60edcd6accead222
Yusein Ali <yuse_91yus@hotmail.com>
2024-12-06 23:20:45 +0000

    Style sign.up page

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['8000-yuss76a-djangoblogy-uiku0d6mso8.ws.codeinstitute-ide.net',
 '.herokuapp.com']
 
